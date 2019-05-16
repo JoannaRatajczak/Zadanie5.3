cc96d2e0c01c2d4e65ce62727b4c229566da0e79
JoannaRatajczak <joanna.jr.ratajczak@gmail.com>
2019-05-16 23:32:44 +0200

    poprawna wersja edyt.

--- src/Check.java
+++ src/Check.java
@@ -1,21 +1,21 @@
 public class Check {
 
-    Line distance (Line line1, Line line2){
-        double line1lenght =lenght(line1);
-        double line2lenght =lenght(line2);
-        if (line1lenght>line2lenght)
+    Line distance(Line line1, Line line2) {
+        double line1lenght = lenght(line1);
+        double line2lenght = lenght(line2);
+        if (line1lenght > line2lenght)
             return line1;
         else return line2;
 
     }
 
     private double sqr(double a) {
-        return a*a;
+        return a * a;
     }
 
-    double lenght (Line line){
+    double lenght(Line line) {
         double odc;
-        odc = (sqr(line.point1.a-line.point1.b)+sqr(line.point2.a-line.point2.b))*(0.5);
+        odc = (sqr(line.point1.a - line.point1.b) + sqr(line.point2.a - line.point2.b)) * (0.5);
         return odc;
     }
 
--- src/Test.java
+++ src/Test.java
@@ -1,16 +1,16 @@
 public class Test {
     public static void main(String[] args) {
-        Point point1 = new Point(5,80);
-        Point point2 = new Point (-5, 50);
+        Point point1 = new Point(5, 80);
+        Point point2 = new Point(-5, 50);
         Line line1 = new Line(point1, point2);
 
-        Point point3 = new Point(0,8);
-        Point point4 = new Point (0, 6);
+        Point point3 = new Point(0, 8);
+        Point point4 = new Point(0, 6);
         Line line2 = new Line(point3, point4);
 
         Check check = new Check();
         Line longerOne = check.distance(line1, line2);
-        System.out.println("Początek: "+longerOne.point1.a +", "+ longerOne.point1.b + " ;koniec: "+longerOne.point2.a +", "+ longerOne.point2.b+" Długość: "+check.lenght(longerOne));
+        System.out.println("Początek: " + longerOne.point1.a + ", " + longerOne.point1.b + " ;koniec: " + longerOne.point2.a + ", " + longerOne.point2.b + " Długość: " + check.lenght(longerOne));
 
 
     }
